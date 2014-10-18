bb78850d3ff754c07c32f8cf9e079a5c281969e8
isuru <ewisuru@gmail.com>
2014-10-18 06:06:29 +0530

    Restricted orientation to protrait only

--- MovementAnalyzer/gen/isuru/apps/movementanalyzer/R.java
+++ MovementAnalyzer/gen/isuru/apps/movementanalyzer/R.java
@@ -30,25 +30,26 @@ public final class R {
         public static final int uom=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080014;
+        public static final int action_settings=0x7f080015;
         public static final int button1=0x7f08000a;
         public static final int button2=0x7f08000c;
         public static final int coordinateManager=0x7f080001;
         public static final int coordinateSendButton=0x7f080007;
-        public static final int demot=0x7f080013;
+        public static final int demot=0x7f080014;
         public static final int editText1=0x7f080006;
         public static final int editText3=0x7f080005;
-        public static final int gridLayout1=0x7f08000f;
-        public static final int image_viewer=0x7f08000e;
-        public static final int listView1=0x7f080012;
+        public static final int gridLayout1=0x7f080010;
+        public static final int imageViewer=0x7f08000d;
+        public static final int image_viewer=0x7f08000f;
+        public static final int listView1=0x7f080013;
         public static final int pager=0x7f080000;
         public static final int radioButton1=0x7f080003;
         public static final int radioButton2=0x7f080004;
         public static final int radioLocationSelectionMethod=0x7f080002;
         public static final int section_label=0x7f080008;
-        public static final int spinner1=0x7f080010;
-        public static final int spinner2=0x7f080011;
-        public static final int test_image=0x7f08000d;
+        public static final int spinner1=0x7f080011;
+        public static final int spinner2=0x7f080012;
+        public static final int test_image=0x7f08000e;
         public static final int textView1=0x7f080009;
         public static final int textView2=0x7f08000b;
     }
